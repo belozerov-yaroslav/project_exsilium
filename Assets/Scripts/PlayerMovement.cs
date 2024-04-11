c845e8150405cb492c54ac9ad88c4ad32e5e341c
mikhrenin_roman <mikhreninfy@gmail.com>
2024-04-11 15:51:09 +0500

    fixed missing files on player movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private CustomInput _input;
+    private Rigidbody2D _rb;
+    private Vector2 _moveVector;
+    private float _moveSpeed = 5f;
+
+    private void Awake()
+    {
+        _input = new CustomInput();
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnEnable()
+    {
+        _input.Enable();
+        _input.Player.Movment.performed += OnMovementPerformed;
+        _input.Player.Movment.canceled += OnMovementCanceled;
+    }
+    
+    private void OnDisable()
+    {
+        _input.Disable();
+        _input.Player.Movment.performed -= OnMovementPerformed;
+        _input.Player.Movment.canceled -= OnMovementCanceled;
+    }
+
+    private void FixedUpdate()
+    {
+        _rb.velocity = _moveVector * _moveSpeed;
+    }
+
+    private void OnMovementPerformed(InputAction.CallbackContext value)
+    {
+        _moveVector = value.ReadValue<Vector2>();
+    }
+    
+    private void OnMovementCanceled(InputAction.CallbackContext value)
+    {
+        _moveVector = Vector2.zero;
+        
+    }
+}
