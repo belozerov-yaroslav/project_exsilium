d52eccbf200ce19f31cfe2ed457c7d11c1f0844e
belozerov-yaroslav <jarblz@yandex.ru>
2024-04-22 23:54:57 +0500

    Немного причесал вывод диалога

---
+++ Assets/InkDialogues/test4.ink
@@ -0,0 +1,6 @@
+INCLUDE globals.ink
+
+
+hello
++ accept quest #quest_give:testQ
++ {testQuest} finish quest #quest_finish:testQ
--- Assets/Scripts/DialogueSystem/DialoguePanel.cs
+++ Assets/Scripts/DialogueSystem/DialoguePanel.cs
@@ -14,45 +14,64 @@ public class DialoguePanel : MonoBehaviour
     [SerializeField] float btnTextMargin = 5f;
     private List<GameObject> currentMessages = new List<GameObject>();
     private List<GameObject> currentChoiceButtons = new List<GameObject>();
-    private float lastMessagePos = 0;
+    private Stack<float> lastMessagePos = new(new float[] { 0 });
+
     private float contentHeight = 0;
+
     private void Awake()
     {
         if (instance != null)
         {
             Debug.LogWarning("Found more than one Dialogue Panel in the scene");
         }
+
         instance = this;
     }
+
     private void Start()
     {
     }
-    public static DialoguePanel GetInstance() { return instance; }
+
+    public static DialoguePanel GetInstance()
+    {
+        return instance;
+    }
+
     public void Show()
     {
         gameObject.SetActive(true);
     }
+
     public void Hide()
     {
         gameObject.SetActive(false);
         DeleteChoiceButtons();
         DeleteAllMessages();
     }
+
     public void DisplayMessage(string message)
     {
         if (message == "")
             return;
-        GameObject newTextElement = Instantiate(textExample, content.transform);
-        newTextElement.SetActive(true);
-        newTextElement.transform.localPosition = new Vector3(newTextElement.transform.localPosition.x, lastMessagePos - spaceBetweenMessages, 0);
 
+        GameObject newTextElement = Instantiate(textExample, content.transform);
         TextMeshProUGUI textElem = newTextElement.GetComponent<TextMeshProUGUI>();
+        
         textElem.text = message;
-        lastMessagePos = newTextElement.transform.localPosition.y - textElem.GetPreferredValues().y;
-        AdjustContentSize(textElem.GetPreferredValues().y + spaceBetweenMessages);
+        newTextElement.SetActive(true);
+        AdjustTransformToTextSize(textElem);
+        var textHeight = textElem.rectTransform.rect.height;
+
+        var localPosition = newTextElement.transform.localPosition;
+        localPosition = new Vector3(localPosition.x, lastMessagePos.Peek() - spaceBetweenMessages - textHeight / 2, 0);
+        newTextElement.transform.localPosition = localPosition;
+        
+        lastMessagePos.Push(localPosition.y - textHeight / 2);
+        AdjustContentSize(textHeight + spaceBetweenMessages);
 
         currentMessages.Add(newTextElement);
     }
+
     public void DisplayChoices(List<string> choices)
     {
         int index = 0;
@@ -60,48 +79,70 @@ public class DialoguePanel : MonoBehaviour
         {
             GameObject choiceBtn = Instantiate(choiceBtnExample, content.transform);
             choiceBtn.SetActive(true);
-            choiceBtn.transform.localPosition = new Vector3(choiceBtn.transform.localPosition.x, -contentHeight - spaceBetweenMessages, 0);
+
+            TextMeshProUGUI btnText = choiceBtn.GetComponentInChildren<TextMeshProUGUI>();
+            btnText.text = choice;
+            AdjustTransformToTextSize(btnText);
+            var textHeight = btnText.rectTransform.rect.height;
+            choiceBtn.GetComponent<RectTransform>()
+                .SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, textHeight + 2 * btnTextMargin);
+
+            choiceBtn.transform.localPosition = new Vector3(choiceBtn.transform.localPosition.x,
+                lastMessagePos.Peek() - spaceBetweenMessages - btnTextMargin - textHeight / 2, 0);
 
             var i = index; // c# - гений, эта строка нужна для обхода "особенностей языка", НЕ УБИРАТЬ
             choiceBtn.GetComponent<Button>().onClick.AddListener(() => ButtonClicked(i));
             index += 1;
 
-            TextMeshProUGUI btnText = choiceBtn.GetComponentInChildren<TextMeshProUGUI>();
-            btnText.text = choice;
-            float textHeight = btnText.GetPreferredValues().y;
-            choiceBtn.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, textHeight + 2 * btnTextMargin);
+
             AdjustContentSize(spaceBetweenMessages + textHeight + 2 * btnTextMargin);
+            lastMessagePos.Push(lastMessagePos.Peek() - spaceBetweenMessages - (textHeight + 2 * btnTextMargin));
 
             currentChoiceButtons.Add(choiceBtn);
         }
     }
+
+    private void AdjustTransformToTextSize(TextMeshProUGUI text)
+    {
+        text.ForceMeshUpdate();
+        var textHeight = text.GetRenderedValues().y;
+        text.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, textHeight);
+    }
+
     private void AdjustContentSize(float adjustSize)
     {
         contentHeight += adjustSize;
         content.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, contentHeight);
     }
+
     public void ButtonClicked(int index)
     {
         DeleteChoiceButtons();
         DialogueManager.GetInstance().MakeChoice(index);
     }
+
     private void DeleteChoiceButtons()
     {
         foreach (GameObject choiceButton in currentChoiceButtons)
         {
             Destroy(choiceButton);
+            lastMessagePos.Pop();
         }
+
+
         currentChoiceButtons.Clear();
-        AdjustContentSize(-(contentHeight + lastMessagePos));
+        AdjustContentSize(-(contentHeight + lastMessagePos.Peek()));
     }
+
     private void DeleteAllMessages()
     {
         foreach (GameObject message in currentMessages)
         {
             Destroy(message);
         }
+
         currentMessages.Clear();
-        lastMessagePos = 0;
+        lastMessagePos.Push(0);
         AdjustContentSize(-contentHeight);
     }
 }
