ea9dc30ee72dbd4ecdcd160125d3ea7fae6a491c
belozerov-yaroslav <jarblz@yandex.ru>
2024-05-17 11:33:04 +0500

    fixs

--- Assets/Scripts/InGameObjects/SecondLevel/HandleBanishFinishedSecondLevel.cs
+++ Assets/Scripts/InGameObjects/SecondLevel/HandleBanishFinishedSecondLevel.cs
@@ -2,14 +2,16 @@ using System;
 using BanishSystem;
 using UnityEngine;
 
-public class HandleBanishFinished : MonoBehaviour
+public class HandleBanishFinishedSecondLevel : MonoBehaviour
 {
+    [SerializeField] private GameObject _babaika;
     private void Start()
     {
         BanishManager.BanishFinished += () =>
         {
             DialogueManager.instance.SetVariableState("liho_banished", new Ink.Runtime.BoolValue(true));
             DoorEnter.IsBanishComplete = true;
+            Destroy(_babaika);
         };
     }
 }
