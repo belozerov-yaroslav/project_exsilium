d3a46ca38882e8b6e265b025f746a60e83b659b7
mikhrenin_roman <mikhreninfy@gmail.com>
2024-05-20 15:14:36 +0500

    Добавил префаб слайд менеджера и подсказку про скип слайдов фикс коммита

--- Assets/Scripts/Slides/SlideManager.cs
+++ Assets/Scripts/Slides/SlideManager.cs
@@ -11,6 +11,10 @@ public class SlideManager : MonoBehaviour
     [SerializeField] private Canvas slideCanvas;
     [SerializeField] private Image previousSlide;
     [SerializeField] private Image currentSlide;
+    [SerializeField] private Image instructionImage;
+    [SerializeField] private float instructionTransparency = 0.6f;
+    [SerializeField] private float instructionDelay = 2;
+    [SerializeField] private float appearanceTime = 1;
     private int _index;
     private List<Slide> _currentSlides;
     public static SlideManager Instance { get; private set; }
@@ -26,12 +30,14 @@ public class SlideManager : MonoBehaviour
     public void ShowSlideGroup(List<Slide> slides)
     {
         CustomInputInitializer.CustomInput.SlideShow.NextSlide.performed += NextSlide;
+        CustomInputInitializer.CustomInput.SlideShow.NextSlide.performed += TurnOffInstruction;
         slideCanvas.gameObject.SetActive(true);
         GameStateMachine.Instance.StateTransition(SlideShowState.Instance);
         _currentSlides = slides;
         _index = 0;
         previousSlide.color = new Color(0, 0, 0, 1f);
         currentSlide.color = new Color(1, 1, 1, 0f);
+        TurnOnInstruction();
         _currentSlides[0].TurnOn();
     }
 
@@ -52,5 +58,31 @@ public class SlideManager : MonoBehaviour
         _currentSlides[_index - 1].StopAllCoroutines();
         _currentSlides[_index - 1].TurnOff();
         GameStateMachine.Instance.StateTransition(null);
+    }
+
+    private void TurnOnInstruction()
+    {
+        StartCoroutine(FadeInInstructionWithDelay());
+    }
+
+    private void TurnOffInstruction(InputAction.CallbackContext context)
+    {
+        CustomInputInitializer.CustomInput.SlideShow.NextSlide.performed -= TurnOffInstruction;
+        StopAllCoroutines();
+        StartCoroutine(FadeOutInstruction());
+    }
+
+    private IEnumerator FadeInInstructionWithDelay()
+    {
+        yield return new WaitForSeconds(instructionDelay);
+        yield return StartCoroutine(ChangeValueSmooth.Change(instructionImage.color.a, instructionTransparency,
+            value => instructionImage.color = new Color(1, 1, 1, value), appearanceTime));
+    }
+
+
+    private IEnumerator FadeOutInstruction()
+    {
+        yield return StartCoroutine(ChangeValueSmooth.Change(instructionImage.color.a, 0,
+            value => instructionImage.color = new Color(1, 1, 1, value), appearanceTime));
     }
 }
