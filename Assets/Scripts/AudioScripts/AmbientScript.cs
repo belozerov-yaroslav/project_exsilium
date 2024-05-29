b475414c3202b284273d9f35a63cba64df8789e9
Kirill <Vsadnikofhell@mail.ru>
2024-05-29 18:00:27 +0500

    fix

--- Assets/Scripts/AudioScripts/AmbientScript.cs
+++ Assets/Scripts/AudioScripts/AmbientScript.cs
@@ -14,8 +14,12 @@ public class AmbientScript : MonoBehaviour
     [SerializeField] private AudioSource basementAmbient;
     [SerializeField] private AudioSource mansionAmbient;
     
-    private Dictionary<string, AudioSource> _ambientForLevels = new Dictionary<string, AudioSource>();
+    private string LastLevelName { get; set; } 
     
+    private Dictionary<string, AudioSource> _ambientForLevels = new Dictionary<string, AudioSource>();
+    private Dictionary<string, float> _ambientVolumes = new Dictionary<string, float>();
+
+    private List<string> _ambientOnLoad = new List<string>();
     
     private void Awake()
     {
@@ -26,9 +30,27 @@ public class AmbientScript : MonoBehaviour
     private void Start()
     {
         _ambientForLevels["Level 1"] = spbAmbient;
+        _ambientVolumes["Level 1"] = spbAmbient.volume;
         _ambientForLevels["Level2"] = basementAmbient;
+        _ambientVolumes["Level2"] = basementAmbient.volume ;
         _ambientForLevels["Level3"] = spbAmbient;
+        _ambientVolumes["Level3"] = spbAmbient.volume ;
         _ambientForLevels["Level5"] = mansionAmbient;
+        _ambientVolumes["Level5"] = mansionAmbient.volume ;
+        _ambientOnLoad.Add("Level3");
+    }
+
+    public void StartOnLoad(string levelName)
+    {
+        StartCoroutine(nameof(StopAmbient));
+        if (_ambientOnLoad.Contains(levelName))
+        {
+            AppearAmbient(levelName);
+        }
+        else
+        {
+            StopAmbient();
+        }
     }
 
     public void AppearAmbient(string levelName)
@@ -36,18 +58,20 @@ public class AmbientScript : MonoBehaviour
         if(!_ambientForLevels.ContainsKey(levelName))
             return;
         var currentMusic = _ambientForLevels[levelName];
-        var currentVolume = currentMusic.volume; 
+        var currentVolume = _ambientVolumes[levelName]; 
         currentMusic.volume = 0f;
         currentMusic.Play();
         StartCoroutine(ChangeValueSmooth.Change(0f, currentVolume,
             value => currentMusic.volume = value
             , smoothTime, AnimationCurves.ThirdGrade));
+        LastLevelName = levelName;
     }
 
     public void StopAmbient()
     {
-        var currentMusic = _ambientForLevels[SceneManager.GetActiveScene().name];
-        var currentVolume = currentMusic.volume;
+        if(LastLevelName == null || !_ambientForLevels.ContainsKey(LastLevelName)) return;
+        var currentMusic = _ambientForLevels[LastLevelName];
+        var currentVolume = _ambientVolumes[LastLevelName];
         StartCoroutine(ChangeValueSmooth.Change(currentVolume, 0f,
             value => { 
                 currentMusic.volume = value;
@@ -58,4 +82,5 @@ public class AmbientScript : MonoBehaviour
                 } 
             }, smoothTime, AnimationCurves.ThirdGrade));
     }
+    
 }
